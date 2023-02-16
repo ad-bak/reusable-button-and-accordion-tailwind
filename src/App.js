3cad8e9ebfe8da963c76a4aefae8d72a9deb626a
Adnan <adbak.dev@gmail.com>
2023-02-16 18:01:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button secondary outline rounded>
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline rounded>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
